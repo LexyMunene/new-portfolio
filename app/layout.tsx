cd00e967919aadec86de23675c430371e4744eb0
saisudheer <saisudheer5530>
2025-02-12 11:41:35 +0300

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Inter, Roboto_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Alex's Portfolio",
+  description: "Data Scientist and Software Engineer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${robotoMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,206 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a Python CR Desktop App",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Guitar chords identification ",
+      des: "A Machine Learning Model that identifies guitar chords.",
+      img: "/guitar.jpg",
+      iconLists: ["/MLI.png", "/python.jpeg", "/jupyter.jpeg"],
+      link: "https://github.com/LexyMunene",
+    },
+    {
+      id: 2,
+      title: "HR Dashboard",
+      des: "A dashboard that helps HR managers to manage their employees and their data using tableau.",
+      img: "/hr.svg",
+      iconLists: ["/tableau1.jpeg"],
+      link: "https://public.tableau.com/app/profile/alex.m6808/viz/HRDASHBOARDTableau/HRANALYTICSDASHBOARD",
+    },
+    {
+      id: 3,
+      title: "CR-RECORDS software",
+      des: "A windows software that helps in church data management.",
+      img: "/2c.jpeg",
+      iconLists: ["/MLI.png", "/python.jpeg"],
+      link: "https://github.com/LexyMunene",
+    },
+    {
+      id: 4,
+      title: "ARIMAA GAME",
+      des: "An android game that allows two players to play against each other.",
+      img: "/Arimaa.png",
+      iconLists: ["/android.png", "/AS.jpeg", ],
+      link: "https://github.com/LexyMunene",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Very quick response time , patient , understanding great communication great efforts , really goes above and beyond",
+      name: "Thinkbig",
+      title: "Model",
+    },
+    {
+      quote:
+        "Very quick response time , patient , understanding great communication great efforts , really goes above and beyond",
+      name: "Thinkbig",
+      title: "Model",
+    },
+    {
+      quote:
+        "Very quick response time , patient , understanding great communication great efforts , really goes above and beyond",
+      name: "Thinkbig",
+      title: "Model",
+    },
+    
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Data Analyst Intern - Delloite",
+      desc: "Analyzed data to identify trends and patterns, and presented findings to the team.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Machine Learning Dev - Fiverr",
+      desc: "Developed a chord identification model using machine learning algorithms.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained websites & Portfolios using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
